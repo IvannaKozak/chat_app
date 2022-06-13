369147e75b3a210d4fb56c355bce04c74bd9f608
IvannaKozak <ivanka.kozak3@gmail.com>
2022-06-13 15:08:57 +0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:chat_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:chat_app/screens/registration_screen.dart';
+import 'package:chat_app/screens/chat_screen.dart';
+
+import 'screens/welcome_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          //bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        ChatScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => WelcomeScreen(),
+        RegistrationScreen.id: (context) => WelcomeScreen(),
+      },
+    );
+  }
+}
